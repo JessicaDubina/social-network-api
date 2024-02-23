06b343c9c654b16b0024b1f6f67ad486c0aa7747
jdub <jessicaclaing@gmail.com>
2024-02-22 18:01:53 -0800

    created api routes files for users and thoguhts. added boilerplate route modularization to index files.

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+//import queries from controllers
+
+// api/users - get all users
+
+// api/users/:id - get a user by _id
+
+// api/users - post new user
+
+// api/users/:id - put to update user by _id
+
+// api/users/:id - delete user by _id
+    //remove assoviated thoughts when deleted
+
+module.exports = router;
