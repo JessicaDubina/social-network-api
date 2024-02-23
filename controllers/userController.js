f894a45364fa9fc5247e8d05c1b4d1437e2dd5f0
jdub <jessicaclaing@gmail.com>
2024-02-22 21:32:35 -0800

    added user getOne, update, and delete routes and controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,8 @@ module.exports = {
         .catch((err) => res.status(500).json(err));
     },
     getUser(req, res) {
-        User.findById({_id: req.params.userId})
+        let userId = req.params.userId;
+        User.findById(userId)
         .then((user) =>
             !user
             ? res.status(404).json({ message: 'No user with that ID' })
@@ -20,4 +21,47 @@ module.exports = {
         .then((userData) => res.json(userData))
         .catch((err) => res.status(500).json(err));
     },
+    updateUser(req, res) {
+        let userId = req.params.userId;
+        User.findOneAndUpdate(
+            {_id: userId},
+            {
+                username: req.body.username,
+                email: req.body.email,
+                thoughts: req.body.thoughts,
+                friends: req.body.friends
+            },
+            {new: true },
+        )
+        .then(result => {
+            if (result) {
+                res.status(200).json(result);
+                console.log(`Updated: ${result}`);
+            } else {
+                console.log('Uh Oh, something went wrong');
+                res.status(500).json({ message: 'something went wrong' });
+            }
+        })
+        .catch(err => {
+            console.error('Error updating user:', err);
+            res.status(500).json({ message: 'something went wrong' });
+        })    
+    },
+    deleteUser(req, res) {
+        let userId = req.params.userId;
+        User.findByIdAndDelete(userId)
+        .then(result => {
+            if (result) {
+                res.status(200).json(result);
+                console.log(`Deleted: ${result}`);
+            } else {
+                console.log('Uh Oh, something went wrong');
+                res.status(500).json({ message: 'something went wrong' });
+            }
+        })
+        .catch(err => {
+            console.error('Error deleting user:', err);
+            res.status(500).json({ message: 'something went wrong' });
+        })   
+    }
 };
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
-const { getAllUsers, getUser, newUser } = require('../../controllers/userController');
+const { getAllUsers, getUser, newUser, updateUser, deleteUser } = require('../../controllers/userController');
 
 // api/users - get all users, post new user
 router.route('/').get(getAllUsers).post(newUser);
 
-// api/users/:id - get a user by _id
+// api/users/:id - get a user by id, update a user by id, delete a user by id
+router.route('/:userId').get(getUser).post(updateUser).delete(deleteUser);
 
-// api/users/:id - put to update user by _id
-
-// api/users/:id - delete user by _id
-    //remove assoviated thoughts when deleted
+    //remove associated thoughts when deleted
 
 module.exports = router;
