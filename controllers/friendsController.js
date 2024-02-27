d06dfdfd2cd21a9b9360496d0b77ae5b472d325e
jdub <jessicaclaing@gmail.com>
2024-02-26 16:56:31 -0800

    combined user and friend routes files. added friend controller functions.

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -1,11 +1,51 @@
 const User = require('../models/User');
 
 module.exports = {
+    //add a new friend to a user's friend list
     addFriend(req, res) {
-        //add a new friend to a user's friend list
+        let userId = req.params.userId;
+        let friendId = req.params.friendId;
+        User.findOneAndUpdate(
+            {_id: userId},
+            { $addToSet: { friends: friendId } },
+            { new: true }
+        )
+        .then(result => {
+            if (result) {
+                res.status(200).json(result);
+                console.log(`Added friend: ${result}`);
+            } else {
+                console.log('No user with that ID');
+                res.status(404).json({ message: 'No user with that ID' });
+            }
+        })
+        .catch(err => {
+            console.error('Error updating user:', err);
+            res.status(500).json({ message: 'something went wrong' });
+        })    
     },
+    //remove a friend from a user's friend list
     removeFriend(req, res) {
-        //remove a friend from a user's friend list
+        let userId = req.params.userId;
+        let friendId = req.params.friendId;
+        User.findOneAndUpdate(
+            {_id: userId},
+            { $pull: { friends: friendId } },
+            { new: true }
+        )
+        .then(result => {
+            if (result) {
+                res.status(200).json(result);
+                console.log(`Removed friend: ${result}`);
+            } else {
+                console.log('No user with that ID');
+                res.status(404).json({ message: 'No user with that ID' });
+            }
+        })
+        .catch(err => {
+            console.error('Error updating user:', err);
+            res.status(500).json({ message: 'something went wrong' });
+        })    
     }
 
 }
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { getAllUsers, getUser, newUser, updateUser, deleteUser } = require('../../controllers/userController');
+const { addFriend, removeFriend } = require('../../controllers/friendsController');
 
 // api/users - get all users, post new user
 router.route('/').get(getAllUsers).post(newUser);
@@ -7,6 +8,7 @@ router.route('/').get(getAllUsers).post(newUser);
 // api/users/:id - get a user by id, update a user by id, delete a user by id
 router.route('/:userId').get(getUser).post(updateUser).delete(deleteUser);
 
-    //remove associated thoughts when deleted
+// /api/users/:userId/friends/:friendId - post new friend, delete friend
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend)
 
 module.exports = router;
